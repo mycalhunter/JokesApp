cf4c91ca51242a7ac45e44f2ee04118f5e00bff9
Hunter Miller <hunter.miller@levolor.com>
2021-10-08 18:02:52 -0400

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace JokesApp.Models
+{
+    public class Joke
+    {
+
+        public int Id { get; set; } // prop tab tab to create
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+        public Joke() // ctor tab tab to create constructor
+        {
+
+        }
+    }
+}

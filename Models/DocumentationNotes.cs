28a53272c3e162d3ef5a834a5848455b37d4d699
Hunter Miller <hunter.miller@levolor.com>
2021-10-11 15:58:49 -0400

    change switch statement to switch expression, add await for async

--- Models/DocumentationNotes.cs
+++ Models/DocumentationNotes.cs
@@ -18,52 +18,23 @@ namespace JokesApp.Models
         public static string Holiday()  {
                 DateTime today = DateTime.Today; // get todays date
                 string holiday = today.Month.ToString(); // convert int month to string
-                string currentHoliday = ""; // set current holiday variable
-                var builder = new HtmlContentBuilder(); // create HtmlBuilder
-
-                switch (holiday)
+                string currentHoliday = holiday switch
                 {
-                    case "1":
-                        currentHoliday = "New Years Day";
-                        break;
-                    case "2":
-                        currentHoliday = "Valentines Day";
-                        break;
-                    case "3":
-                        currentHoliday = "St.Patrick's Day";
-                        break;
-                    case "4":
-                        currentHoliday = "Arbor Day";
-                        break;
-                    case "5":
-                        currentHoliday = "Memorial Day";
-                        break;
-                    case "6":
-                        currentHoliday = "Native American Day";
-                        break;
-                    case "7":
-                        currentHoliday = "Independence Day";
-                        break;
-                    case "8":
-                        currentHoliday = "Coast Gaurd Birthday";
-                        break;
-                    case "9":
-                        currentHoliday = "Labor Day";
-                        break;
-                    case "10":
-                        currentHoliday = "Halloween";
-                        break;
-                    case "11":
-                        currentHoliday = "Thanksgiving Day";
-                        break;
-                    case "12":
-                        currentHoliday = "Christmas Day";
-                        break;
-                    default:
-                        currentHoliday = "You are living outside of the Gregorian calendar, are you an Alien ?";
-                        break;
-                }
-            return currentHoliday;
+                    "1" => "New Years Day",
+                    "2" => "Valentines Day",
+                    "3" => "St.Patrick's Day",
+                    "4" => "Arbor Day",
+                    "5" => "Memorial Day",
+                    "6" => "Native American Day",
+                    "7" => "Independence Day",
+                    "8" => "Coast Guard Birthday",
+                    "9" => "Labor Day",
+                    "10" => "Halloween",
+                    "11" => "Thanksgiving Day",
+                    "12" => "Christmas Day",
+                    _ => "You are living outside of the Gregorian calendar, are you an Alien ?",
+                };
+                return currentHoliday;
         }
 
 

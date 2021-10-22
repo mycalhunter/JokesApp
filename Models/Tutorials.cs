40d3897602f941f1292acff30d60ef5f100393b1
Hunter Miller <hunter.miller@levolor.com>
2021-10-22 15:40:41 -0400

    fix naming rule violation

--- Models/Tutorials.cs
+++ Models/Tutorials.cs
@@ -10,12 +10,12 @@ namespace JokesApp.Models
         public int StudentId { get; set; } // hidden field
         public string StudentName { get; set; } // Text Box
         public int Age { get; set; }
-        public bool isNewlyEnrolled { get; set; }
+        public bool IsNewlyEnrolled { get; set; }
         public DateTime DoB { get; set; }
         public Gender StudentGender { get; set; } // dropdown
         public string GenderRadioMale { get; set; } // radio
         public string GenderRadioFemale { get; set; } // radio
-        public bool isActive { get; set; } // checkbox
+        public bool IsActive { get; set; } // checkbox
         public string Description { get; set; } // Text Area
         public string Password { get; set; }
         public string StudentSubject { get; set; }
